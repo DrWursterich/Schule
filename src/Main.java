371d0510c635a313880a74bc49e1b08f7ae39130
Admin <Admin@192.168.0.199>
2018-01-13 23:00:39 +0100

    debugging gitignore

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		//TicTacToe.TicTacToe game = new TicTacToe.TicTacToe(new TicTacToe.Player("Spieler 1", 'X'), new TicTacToe.Player("Spieler 2", 'O'));
-		VierGewinnt.VierGewinnt game = new VierGewinnt.VierGewinnt(new VierGewinnt.Player("Spieler 1", 'X'), new VierGewinnt.Player("Spieler 2", 'O'));
+		TicTacToe.TicTacToe game = new TicTacToe.TicTacToe(new TicTacToe.Player("Spieler 1", 'X'), new TicTacToe.Player("Spieler 2", 'O'));
+		//VierGewinnt.VierGewinnt game = new VierGewinnt.VierGewinnt(new VierGewinnt.Player("Spieler 1", 'X'), new VierGewinnt.Player("Spieler 2", 'O'));
 		String input = "Y";
 		while(input.equals("Y") || input.equals("y")) {
 			game.play();

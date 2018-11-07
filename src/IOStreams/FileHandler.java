80f3d7080830fee4a9150897651d32b56efb873b
schaepermario <schaepermario@R166PC08.schule.local>
2018-11-07 12:08:19 +0100

    Added functions

--- src/IOStreams/FileHandler.java
+++ src/IOStreams/FileHandler.java
@@ -4,9 +4,27 @@ import java.io.File;
 import java.io.IOException;
 
 public class FileHandler {
-	public void createFile(String filename) throws IOException {
+	public static void createFile(String filename) throws IOException {
 		if (!(new File(filename)).createNewFile()) {
 			throw new IOException("Unable to create file " + filename);
+		}
+	}
+
+	public static void deleteFile(String filename) throws IOException {
+		File file = new File(filename);
+
+		if (file.isDirectory()) {
+			throw new IOException("File is a directory");
+
+		}
+		if (!file.delete()) {
+			throw new IOException("Unable to delete file " + filename);
+		}
+	}
+	
+	public static void createDirectory(String directoryname) throws IOException {
+		if (!(new File(directoryname)).mkdir()) {
+			throw new IOException("Unable to create directory" + directoryname);
 		}
 	}
 }
